e573581f0b7ba4c3e8cc8f3ed9b69c5d62fd9ba1
Quan Huynh <quanhuynh364@gmail.com>
2026-02-06 13:58:37 -0500

    added lab2 c file

---
+++ lab2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define BSA1 0.007184
+#define BSA2 0.425
+#define BSA3 0.725
+#include <math.h>
+
+
+int main(){
+    float height, weight;
+    double res;
+    char input[50];
+
+    printf("Please enter your weight(kg) and height(cm): \n");
+    fgets(input, sizeof(input),stdin);
+    sscanf(input, "%f %f", &weight, &height);
+
+    res = BSA1 * pow(weight, BSA2) * pow(height, BSA3);
+    printf("The height is: %.1f centimeters\n", height);
+    printf("The weight is: %.1f kilograms\n", weight );
+    printf("BSA: %.4f meter squares\n", res);
+
+    return 0;
+}
