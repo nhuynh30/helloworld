8a2e308196b5ecdeefe646a9370b224dd1214df6
Quan Huynh <quanhuynh364@gmail.com>
2026-02-26 11:12:27 -0500

    updated

--- hello.c
+++ hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int main()
 {
+    int timestamp = 0;
+    int temp = 28;
+    int battery = 100;
+    int orient_err = 0;
+
     char buffer[69];
     int seed;
 
@@ -9,5 +16,26 @@ int main()
     fgets(buffer, sizeof(buffer), stdin);
     sscanf(buffer, "%d", &seed);
     srand(seed);
+    printf("random number test: %d\n", seed);
+    printf("Random number is %d\n", rand());
+
+    while (1)
+    {
+        char buffer2[67];
+        char option;
+
+        printf("Enter your option: L, R, T, or Q\n");
+        fgets(buffer2, sizeof(buffer2), stdin);
+        sscanf(buffer2, "%s", &option);
+        option = toupper(option);
+
+        if (option != 'L' && option != 'R' && option != 'T' && option != 'Q')
+        {
+            printf("You mess up the value somehomhow\n");
+            continue;
+        }
+        break;
+    }
+
     return 0;
 }
